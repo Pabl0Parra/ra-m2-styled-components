4ee1a01b2b14cd998ba0d2b4e55990b46c8b0d64
Pabl0Parra <pabsblue16@gmail.com>
2022-12-22 13:32:52 +0100

    updated color palette

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,9 +1,16 @@
-export const colors = {
+const colors = {
   main: '#FF0000',
   secondary: '#0088CC',
+  lightBlue: '#ADD8E6',
+  darkBlue: '#0073E6',
+  lightGreen: '#AEDEC1',
+  purple: '#8A82AF',
+  transparent: 'rgb(0,0,0,0)',
+  red: '#d80202',
   font: {
-    base: '#444',
-    headings: '#121212',
+    base: '#6d6d6d',
+    headings: '#a53636',
+    white: '#FFFFFF',
   },
 }
 
