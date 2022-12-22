fc9d6f85810c57580662ae0ab7882793d2b1e521
Pabl0Parra <pabsblue16@gmail.com>
2022-12-22 22:47:50 +0100

    created HouseCard component

---
+++ src/components/molecules/FirstHouseCard.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable no-undef */
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { colors, dimensions, FlexBox } from '../../styles'
+import { Text, Card } from '../atoms/index'
+import { Title } from './index'
+
+const HouseImage = styled.img`
+  height: 100%;
+  width: 100%;
+  border-radius: 5px;
+  object-fit: cover;
+`
+const FirstHouseCardBody = styled(FlexBox)`
+  width: 100%;
+  padding: 10px;
+  flex-direction: row;
+  align-items: center;
+  position: absolute;
+
+  ${Text} {
+    color: ${colors.font.white};
+    font-size: ${dimensions.font.base};
+    font-weight: bold;
+  }
+`
+
+function FirstHouseCard({ img, name, text }) {
+  return (
+    <Card>
+      <HouseImage src={img} alt={name} />
+      <FirstHouseCardBody>
+        <Title text={name} />
+        <Text>{text}</Text>
+      </FirstHouseCardBody>
+    </Card>
+  )
+}
+
+FirstHouseCard.propTypes = {
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+}
+
+export default FirstHouseCard
---
+++ src/components/molecules/HouseCard.jsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { colors, dimensions, FlexBox } from '../../styles'
+import { Text, Button, Card } from '../atoms/index'
+import { Title } from './index'
+
+const HouseImage = styled.img`
+  height: 56%;
+  width: 100%;
+  border-radius: 5px;
+  object-fit: cover;
+`
+
+const HouseCardBody = styled(FlexBox)`
+  padding: 0.4rem;
+
+  ${Text} {
+    opacity: 0.8;
+  }
+`
+function HouseCard({ img, name, text, price }) {
+  return (
+    <Card>
+      <HouseImage src={img} alt={name} />
+      <HouseCardBody>
+        <Title
+          as="h1"
+          fontSize={`${dimensions.sm}`}
+          text={name}
+          color={`${colors.font.base}`}
+        >
+          {text}
+        </Title>
+        <FlexBox direction="row" justify="space-between" align="center">
+          <Text color={colors.black} fontSize={dimensions.sm} fontWeight="bold">
+            {price}
+          </Text>
+          <Button
+            text="Localizar"
+            fontSize={dimensions.sm}
+            buttonStyles={{
+              height: '50%',
+              width: '45%',
+              backgroundColor: `${colors.main}`,
+            }}
+          />
+        </FlexBox>
+      </HouseCardBody>
+    </Card>
+  )
+}
+
+HouseCard.propTypes = {
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+}
+
+export default styled(HouseCard)``
