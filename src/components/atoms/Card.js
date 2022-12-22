8ca3c19b6c025298b6c47d8db5d05c91fce8d75a
Pabl0Parra <pabsblue16@gmail.com>
2022-12-22 18:11:23 +0100

    minor changes

--- src/components/atoms/Card.js
+++ src/components/atoms/Card.js
@@ -3,8 +3,8 @@ import styled from 'styled-components'
 const Card = styled.div`
     height: ${(height) => height || '220px'}
     width: ${(width) => width || '200px'}
-    overflow: hidden
     border-radius: ${(borderRadius) => borderRadius || '6px'}
+    overflow: hidden    
     box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.32)
 `
 
