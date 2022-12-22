58072edb6508c1d545aa13ca9785436a376526da
Pabl0Parra <pabsblue16@gmail.com>
2022-12-22 17:14:36 +0100

    updated Text.js component

--- src/components/atoms/Text.js
+++ src/components/atoms/Text.js
@@ -4,6 +4,8 @@ import { colors, dimensions } from '../../styles'
 const Text = styled.p`
   color: ${({ color }) => color || colors.font.base};
   font-size: ${({ fontSize }) => fontSize || dimensions.font.base};
+  font-weight: ${({ fontWeight }) => fontWeight || 'regular'};
+  margin: ${({ margin }) => margin || '0.5rem'};
 `
 
 export default Text
