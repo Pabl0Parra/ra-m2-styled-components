c6837ee15699282479ef50d267f90607cfb58765
Pabl0Parra <pabsblue16@gmail.com>
2022-12-23 12:31:50 +0100

    created SearchBar component & updated Icon component + indexes

---
+++ src/components/molecules/SearchBar.jsx
@@ -0,0 +1,15 @@
+import { InputText, Icon, Button } from '../atoms/index'
+
+function SearchBar() {
+  return (
+    <>
+      <InputText placeholder="Piso, chalet o garaje..." />
+      <InputText placeholder="Madrid, Barcelona o Zaragoza..." />
+      <Button>
+        <Icon name="search" size={27} />
+      </Button>
+    </>
+  )
+}
+
+export default SearchBar
