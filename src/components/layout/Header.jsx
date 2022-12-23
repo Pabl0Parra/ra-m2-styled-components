7bc4bf271c60be3de2ccd4cd9927a034368c6d7d
Pabl0Parra <pabsblue16@gmail.com>
2022-12-23 12:16:30 +0100

    updated Header component & added react-router-dom

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,23 +1,41 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
-import { FlexBox } from '../../styles'
+import { colors, FlexBox } from '../../styles/index'
+import { Title } from '../molecules/index'
 
 const HeaderStyled = styled(FlexBox)`
   padding-left: 2rem;
   padding-right: 2rem;
+
+  ul {
+    list-style: none;
+    column-gap: 0.6rem;
+  }
+`
+
+const StyledList = styled(FlexBox)`
+  list-style: none;
+  column-gap: 0.6rem;
+  a {
+    color: ${colors.font.base};
+    text-decoration: none;
+  }
+`
+const StyledLink = styled(NavLink)`
+  &.active {
+    font-weight: bold;
+  }
 `
 
 function Header() {
   return (
     <HeaderStyled direction="row" justify="space-between">
-      <div>MIPISO.com.es</div>
-      <div>
-        <ul>
-          <li>Buscador</li>
-          <li>Datos</li>
-          <li>Mi Perfil</li>
-        </ul>
-      </div>
+      <Title text="MIPISO.com.es" color={colors.font.headings} />
+      <StyledList as="ul" direction="row">
+        <StyledLink to="/">Buscador</StyledLink>
+        <StyledLink to="/data">Datos</StyledLink>
+        <StyledLink to="/profile">Mi Perfil</StyledLink>
+      </StyledList>
     </HeaderStyled>
   )
 }
