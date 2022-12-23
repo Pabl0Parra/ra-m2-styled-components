5c9c3e6666b95500d320ec0151e263f47f8614d3
Pabl0Parra <pabsblue16@gmail.com>
2022-12-23 17:16:43 +0100

    updated Button

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,21 +1,48 @@
 import React from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import { colors } from '../../styles'
 
-export default function Button({
+const StyledButton = styled.button`
+  background-color: ${(props) => props.backgroundColor};
+  border-radius: 5px;
+  padding: 3px 12px;
+  border: none;
+`
+
+const ButtonText = styled(Text).attrs({
+  as: 'span',
+})`
+  color: ${(props) => props.color};
+  font-size: ${(props) => props.fontSize}px;
+`
+
+function Button({
   children,
   color = colors.main,
+  fontSize = 14,
+  backgroundColor,
   onClick = () => {},
 }) {
   return (
-    <button type="button" onClick={onClick} style={{ backgroundColor: color }}>
-      {children}
-    </button>
+    <StyledButton
+      type="button"
+      onClick={onClick}
+      backgroundColor={backgroundColor}
+    >
+      <ButtonText fontSize={fontSize} color={color}>
+        {children}
+      </ButtonText>
+    </StyledButton>
   )
 }
 
 Button.propTypes = {
-  children: PropTypes.node,
+  fontSize: PropTypes.number,
   color: PropTypes.string,
+  children: PropTypes.node,
+  backgroundColor: PropTypes.string,
   onClick: PropTypes.func,
 }
+
+export default styled(Button)``
