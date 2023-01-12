06b4ad64412911ed4a5e74a554db7935b95a195b
Pabl0Parra <pabsblue16@gmail.com>
2023-01-12 14:19:06 +0100

    refactor SelectGroup map

--- src/components/molecules/SelectGroup.jsx
+++ src/components/molecules/SelectGroup.jsx
@@ -2,35 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Label, Select } from '../atoms/index'
 
-export default function SelectGroup({ id, rentalType, city, placeholder }) {
+export default function SelectGroup({ id, options, placeholder }) {
   return (
     <>
-      <Label id={id}>{rentalType}</Label>
+      <Label htmlFor={id} />
       <Select id={id}>
         <option value="" hidden>
           {placeholder}
         </option>
-        {rentalType !== undefined && rentalType.length > 0
-          ? rentalType.map((item) => (
-              <option key={item.index} value={item.property}>
-                {item.property}
-              </option>
-            ))
-          : city.map((item) => (
-              <option key={item.index} value={item.city}>
-                {item.city}
-              </option>
-            ))}
+        {options.map((item) => (
+          <option key={item.id}>{item.value}</option>
+        ))}
       </Select>
     </>
   )
 }
 SelectGroup.propTypes = {
   id: PropTypes.string,
-  rentalType: PropTypes.arrayOf(
-    PropTypes.oneOfType([PropTypes.number, PropTypes.object, PropTypes.string]),
-  ),
-  city: PropTypes.arrayOf(
+  options: PropTypes.arrayOf(
     PropTypes.oneOfType([PropTypes.number, PropTypes.object, PropTypes.string]),
   ),
   placeholder: PropTypes.string,
